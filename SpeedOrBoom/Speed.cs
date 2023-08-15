2b3cffd4f900035b2ad4a5566149c12551edec21
Adam Clouthier <capsar273@gmail.com>
2023-08-15 15:58:37 -0400

    Somewhat finished the explosions

--- SpeedOrBoom/Speed.cs
+++ SpeedOrBoom/Speed.cs
@@ -12,9 +12,14 @@ namespace SpeedOrBoom
         public bool fastEnough;
         public bool activateBombFlag;
         public bool bombReset = false;
-        public float minSpeed = 50.0f;
-        public float radius = 100.0F;
-        public float power = 10000.0F;
+        public float minSpeed = 60.0f;
+        public float radius = 150.0F;
+        public float whimpPower = 100.0F;
+        private GUIStyle redStyle = null;
+        private GUIStyle greenStyle = null;
+        private GUIStyle blueStyle = null;
+        //To be used in the future so player can toggle the type of bomb
+        public float crazyPower = 30.0F;
 
         public override string ID => "8723";
         public override string Name => "Speed or Boom";
@@ -23,39 +28,86 @@ namespace SpeedOrBoom
 
         public override void OnLoad()
         {
-            Debug.Log("SOB: Mod Loaded");
             if (mainscript.M.load)
                 return;
         }
-
         public override void OnGUI()
         {
+            InitStyles();
+            #if DEBUG
             if ( mainscript.M.player.lastCar != null)
             {
                 GUI.Label(new Rect(
-                100, 100, // offset from the top-left of the screen
+                100, 50, // offset from the top-left of the screen
                 1000, 700), // maximum pixel size of the box
                 string.Format("<color=red><size=25>Minimum Speed: {0}</size></color>", // format string
                 mainscript.M.player.lastCar.speed)); // format arguments
 
                 GUI.Label(new Rect(
-                100, 200, // offset from the top-left of the screen
+                100, 100, // offset from the top-left of the screen
                 1000, 700), // maximum pixel size of the box
                 string.Format("<color=red><size=35>Bomb Activated: {0}</size></color>", // format string
                 activateBombFlag)); // format arguments
 
                 GUI.Label(new Rect(
-                100, 300, // offset from the top-left of the screen
+                100, 150, // offset from the top-left of the screen
                 1000, 700), // maximum pixel size of the box
                 string.Format("<color=red><size=25>Fast Enough: {0}</size></color>", // format string
                 fastEnough)); // format arguments
-
+            }
                 GUI.Label(new Rect(
-                100, 400, // offset from the top-left of the screen
+                100, 200, // offset from the top-left of the screen
                 1000, 700), // maximum pixel size of the box
                 string.Format("<color=red><size=25>Bomb Reset: {0}</size></color>", // format string
                 bombReset)); // format arguments
+#endif
+            if (mainscript.M.player.lastCar != null)
+            {
+                if(!bombReset)
+                {
+                    GUI.Box(new Rect(25, 150, 25, 25), string.Format("<color=black><size=15>{0}</size></color>", // format string
+                        (int)mainscript.M.player.lastCar.speed), redStyle); 
+                }
+                else if (bombReset && activateBombFlag)
+                {
+                    GUI.Box(new Rect(25, 150, 25, 25), string.Format("<color=yellow><size=15>{0}</size></color>", (int)mainscript.M.player.lastCar.speed), blueStyle);
+                }
+                else if(bombReset)
+                {  
+                    GUI.Box(new Rect(25, 150, 25, 25), string.Format("<color=black><size=15>{0}</size></color>",(int)mainscript.M.player.lastCar.speed), greenStyle);
+                }
             }
+        }
+        private void InitStyles()
+        {
+            if (redStyle == null)
+            {
+                redStyle = new GUIStyle(GUI.skin.box);
+                redStyle.normal.background = MakeTex(2, 2, new Color(1f, 0f, 0f, 1f));
+            }
+            if(greenStyle == null)
+            {
+                greenStyle = new GUIStyle(GUI.skin.box);
+                greenStyle.normal.background = MakeTex(2,2,new Color(0f, 1f, 0f, 1f));
+            }
+            if (blueStyle == null)
+            {
+                blueStyle = new GUIStyle(GUI.skin.box);
+                blueStyle.normal.background = MakeTex(2, 2, new Color(0f, 0f, 1f, 1f));
+            }
+        }
+
+        private Texture2D MakeTex(int width, int height, Color col)
+        {
+            Color[] pix = new Color[width * height];
+            for (int i = 0; i < pix.Length; ++i)
+            {
+                pix[i] = col;
+            }
+            Texture2D result = new Texture2D(width, height);
+            result.SetPixels(pix);
+            result.Apply();
+            return result;
         }
 
         public override void Update()
@@ -64,23 +116,25 @@ namespace SpeedOrBoom
             {
                 if (mainscript.M.player.lastCar != null)
                 {
-                    if(mainscript.M.player.lastCar.speed < 1 && mainscript.M.player.lastCar.speed > 0)
+                    //Pissing resets the bomb activation if you are somewhat standstill in the car
+                    if (mainscript.M.player.pissing && mainscript.M.player.lastCar.speed > 0 && mainscript.M.player.lastCar.speed < 5)
                     {
                         bombReset = true;
                     }
+                    //Player has met the min speed requirement for the bomb to be enabled and to be going fast enough
                     if (mainscript.M.player.lastCar.speed > minSpeed)
                     {
                         activateBombFlag = true;
-                        //Debug.Log("SOB: Bomb activated");
                         fastEnough = true;
                     }
-                    else if (mainscript.M.player.lastCar.speed < minSpeed - .5f)
+                    //Car dipped under the minimum speed, might blow up
+                    else if (mainscript.M.player.lastCar.speed < minSpeed - 1)
                     {
                         fastEnough = false;
-                        //Debug.Log("SOB: Not fast enough!!!");
                         checkExplode();
                     }
                 }
+                
             }
             catch { }
         }
@@ -89,15 +143,9 @@ namespace SpeedOrBoom
             if (activateBombFlag && !fastEnough && bombReset)
             {
                 //Go Boom
-                Debug.Log("SOB: Calling explode()");
                 activateBombFlag = false;
                 explode();
                 bombReset = false;
-            }
-            else
-            {
-                //Debug.Log("SOB: Safe for Now");
-                return;
             }
         }
 
@@ -105,17 +153,21 @@ namespace SpeedOrBoom
         {
             // Get the current position of the player
             Vector3 currentPlayerPosition = mainscript.M.player.Tb.position;
-            Debug.Log("SOB: EXPLODE TIME");
             
-            Collider[] colliders = Physics.OverlapSphere(currentPlayerPosition, 100.0f);
+            Collider[] colliders = Physics.OverlapSphere(currentPlayerPosition, 150.0f);
+
+            //Find all parts in the collision sphere
             foreach (Collider hit in colliders)
             {
                 Rigidbody rb = hit.GetComponent<Rigidbody>();
-                Debug.Log("SOB: Colliders exist");
                 if (rb != null)
                 {
-                    Debug.Log("SOB: Adding Force/Explosion");
-                    rb.AddExplosionForce(power, currentPlayerPosition, radius, 5.0F, ForceMode.VelocityChange);
+                    //Detatch parts from the car and apply the explosion force to them
+                    partscript part = mainscript.M.player.lastCar.gameObject.GetComponentInChildren<partscript>();
+                    part.FallOFf();
+                    //rb.AddExplosionForce(whimpPower, currentPlayerPosition, radius, 100.0F, ForceMode.Impulse);
+
+                    rb.AddExplosionForce(crazyPower, currentPlayerPosition, radius, 2.0F, ForceMode.VelocityChange);
                 }
             }
         }
